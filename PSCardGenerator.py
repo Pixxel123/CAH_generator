2f80a96b987cb5c6e015557fffebe4629867a1c1
Mark Araujo <maraujo123@hotmail.co.uk>
2020-10-13 00:11:48 +0100

    Refactored to keep code DRY

--- PSCardGenerator.py
+++ PSCardGenerator.py
@@ -35,52 +35,28 @@ def template_fill(card_text, card_colour, card_special, card_assignee, card_foot
     psApp.Open(
         r"E:\MyDocuments\Programming\MarkGithub\PhotoshopFiddlesGame\Fiddles_CAH-Portrait.psd")
     doc = psApp.Application.ActiveDocument
-    underscore_pattern = "_______"
-    white_layers = ["WhiteCardFooter", "WhiteCardTextLayer",
-                    "WhiteBG", f"WhiteCardFaceLogo{card_assignee}"]
-    black_layers = ["BlackCardFooter", "BlackCardTextLayer",
-                    "BlackBG", f"BlackCardFaceLogo{card_assignee}"]
-    assignee_names = ['Alex', 'Fiddles', 'Ellie']
-    # # looping through layer groups
-    # if (len(doc.LayerSets) > 0):
-    #     for group in doc.LayerSets:
-    #         print(type(group.layers[1]))
-    #         print(group.layers[1].name)
-    # for layer in doc.Layers if 'White':
-    #     print(layer.Name)
-    # TODO: Look at using layer names to clean up relevant layer hiding code
-    all_layers = [layer.Name for layer in doc.Layers]
-    print([layer for layer in all_layers if "White" not in layer])
-    special_layers = ["BlackPick2Layer", "BlackDraw3Layer"]
-    for name in assignee_names:
-        doc.ArtLayers[f"WhiteCardFaceLogo{name}"].visible = False
-        doc.ArtLayers[f"BlackCardFaceLogo{name}"].visible = False
-    for special_layer in special_layers:
-        doc.ArtLayers[special_layer].visible = False
-    if card_colour == "Black":
-        print("Black card: Hiding white layers...")
-        for white_layer in white_layers:
-            doc.ArtLayers[white_layer].visible = False
-        # set black card layers to be visible
-        for black_layer in black_layers:
-            doc.ArtLayers[black_layer].visible = True
-        if card_text.count(underscore_pattern) < 2 or card_special == 'N/A':
-            print("Using standard template...")
-        if card_text.count(underscore_pattern) == 2 or card_special == 'PICK 2':
-            doc.ArtLayers["BlackPick2Layer"].visible = True
-            print("Using Pick Two template...")
-        if card_text.count(underscore_pattern) == 3 or card_special == 'DRAW 2 PICK 3':
-            doc.ArtLayers["BlackDraw3Layer"].visible = True
-            print("Using Pick Three template...")
-    elif card_colour == "White":
-        print("White card: Hiding black layers")
-        for black_layer in black_layers:
-            doc.ArtLayers[black_layer].visible = False
-        for white_layer in white_layers:
-            doc.ArtLayers[white_layer].visible = True
+    ignore_folders = ['P2B', 'D2P3B']
+    layer_names = [layer.Name for layer in doc.Layers]
+    all_layers = [
+        layer for layer in layer_names if layer not in ignore_folders]
+    # Initial hiding of all layers to handle colour switching
+    for layer in all_layers:
+        doc.ArtLayers[layer].visible = False
+    layers_to_show = [
+        layer for layer in all_layers if card_colour in layer if layer not in ignore_folders]
+    special_layers = {'PICK 2': "BlackPick2Layer",
+                      'DRAW 2 PICK 3': "BlackDraw3Layer"}
+    for layer in layers_to_show:
+        if card_assignee in layer:
+            # Show appropriate face logo layer
+            doc.ArtLayers[layer].visible = True
+        # Show the rest of the layers
+        if "CardFaceLogo" not in layer:
+            doc.ArtLayers[layer].visible = True
+    if card_special != "N/A":
+        doc.ArtLayers[special_layers[card_special]].visible = True
     doc.ArtLayers[f"{card_colour}CardTextLayer"].TextItem.contents = card_text
     doc.ArtLayers[f"{card_colour}CardFooter"].TextItem.contents = card_footer
-    doc.ArtLayers[f"WhiteCardFaceLogo{card_assignee}"].visible = True
     doc.ArtLayers["HiddenLayerWhenFinished"].visible = False
 
     options = win32com.client.Dispatch('Photoshop.ExportOptionsSaveForWeb')
@@ -91,8 +67,9 @@ def template_fill(card_text, card_colour, card_special, card_assignee, card_foot
     print(f"{card_text} -- Exported PNG of {card_colour}_front_{index_number}_{card_assignee}")
 
 
+print("Generating cards...")
 card_json = json_generator()
 for index, card in enumerate(card_json):
     template_fill(card['text'], card['colour'], card['special'],
                   card['assignee'], card['footer'], index)
-print(f"Complete! {len(card_json)} cards generated!")
+print(f"========================\nComplete! {len(card_json)} cards generated!")
